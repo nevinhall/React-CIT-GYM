d3f9817a1fc2e8565a952d94334fc69f9e0691b1
nevinhall <46722804+nevinhall@users.noreply.github.com>
2020-12-09 10:12:57 +0000

    create basic store

--- src/index.js
+++ src/index.js
@@ -3,6 +3,45 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux'
+
+
+
+//Action
+const increment = () => {
+  return{
+    type: 'INCREMENT'
+  }
+}
+
+//Action
+const decrement = () => {
+  return{
+    type: 'DECREMENT'
+  }
+}
+
+//Reducer
+const counter = (state = 0, action) => {
+  switch(action.type){
+    case "INCREMENT":
+      return state +1;
+    case "DECREMENT":
+      return state -1;
+
+  }
+
+}
+
+let store = createStore(counter)
+
+//Display
+store.subscribe(() => console.log(store.getState()))
+
+//Dispatch
+store.dispatch(increment())
+store.dispatch(decrement())
+store.dispatch(decrement())
 
 ReactDOM.render(
   <React.StrictMode>
